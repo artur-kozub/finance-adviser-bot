e247856d3ed0b9fff72a47126263d645ba6bb858
Arthur Kozub <kozub0246@gmail.com>
2025-04-01 20:52:53 +0300

    Remove dist folder from version control

--- dist/common/tg.bot.class.d.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from "node-telegram-bot-api";
-export declare class TelegramBotClass {
-    private _tgBotApi;
-    constructor();
-    sendReport(msg: Message, text: string): Promise<void>;
-}
